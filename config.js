bd483c6b330f9a0f41d4e012f7228fbdf8ac2497
Anna Perkowska <perkowska.a@gmail.com>
2018-04-20 11:16:40 +0200

    Create repo, add content

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '854315080391-0eerqod71ft4s74gb3446kca7re3g619.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'rpMQTPGepZX32lNvz3OEr8ME',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
